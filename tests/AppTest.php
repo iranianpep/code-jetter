27f0065ef08f9454675234c8c0e04336f3d59338
Ehsan Abbasi <iranianpep@gmail.com>
2016-12-15 23:34:35 +1100

    Fix AppTest

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -1,48 +1,48 @@
 <?php
 
-    namespace CodeJetter\tests;
+namespace CodeJetter\tests;
 
-    use CodeJetter\core\App;
-    use CodeJetter\core\Registry;
+use CodeJetter\core\App;
+use CodeJetter\core\Registry;
 
-    // this is to fix Cannot send session cookie - headers already sent
-    @session_start();
+// this is to fix Cannot send session cookie - headers already sent
+@session_start();
 
-    class AppTest extends \PHPUnit_Framework_TestCase
+class AppTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSetGetEnvironment()
     {
-        public function testSetGetEnvironment()
-        {
-            $app = App::getInstance();
+        $app = App::getInstance();
 
-            $app->setEnvironment('dev');
+        $app->setEnvironment('dev');
 
-            $this->assertEquals('dev', $app->getEnvironment());
+        $this->assertEquals('dev', $app->getEnvironment());
 
-            $app->setEnvironment('prod');
+        $app->setEnvironment('prod');
 
-            $this->assertEquals('prod', $app->getEnvironment());
-        }
-
-        public function testGetSingletons()
-        {
-            $app = App::getInstance();
-            $singletons = [
-                'Config' => 'CodeJetter\Config',
-                'Router' => 'CodeJetter\core\Router',
-                'MySQL' => 'CodeJetter\core\database\MySQLDatabase',
-                'Language' => 'CodeJetter\core\Language'
-            ];
-
-            $this->assertEquals($singletons, $app->getSingletons());
-        }
-
-        public function testGetRegistry()
-        {
-            $app = App::getInstance();
-            $app->init('dev');
-
-            $registry = $app->getRegistry();
-
-            $this->assertEquals(Registry::getInstance(), $registry);
-        }
+        $this->assertEquals('prod', $app->getEnvironment());
     }
+
+    public function testGetSingletons()
+    {
+        $app = App::getInstance();
+        $singletons = [
+            'Config' => 'CodeJetter\Config',
+            'Router' => 'CodeJetter\core\Router',
+            'MySQL' => 'CodeJetter\core\database\MySQLDatabase',
+            'Language' => 'CodeJetter\core\Language'
+        ];
+
+        $this->assertEquals($singletons, $app->getSingletons());
+    }
+
+    public function testGetRegistry()
+    {
+        $app = App::getInstance();
+        $app->init('dev');
+
+        $registry = $app->getRegistry();
+
+        $this->assertEquals(Registry::getInstance(), $registry);
+    }
+}
