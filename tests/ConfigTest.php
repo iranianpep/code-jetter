176533e93e173d3d61966a8cdc1fcf28061159c8
Ehsan Abbasi <iranianpep@gmail.com>
2016-12-21 08:13:23 +1100

    update travis config

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -11,23 +11,23 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
 {
     public function testSetGet()
     {
-//        $config = new Config();
-//
-//        $inputOutputs = [
-//            [
-//                'config'   => 'ROOT_NAMESPACE',
-//                'newValue' => 'CodeJetter',
-//            ],
-//        ];
-//
-//        // this is for empty input
-//        //$this->setExpectedException('Exception');
-//
-//        foreach ($inputOutputs as $inputOutput) {
-//            // set with the new value
-//            $config->set($inputOutput['config'], $inputOutput['newValue']);
-//
-//            $this->assertEquals($config->get($inputOutput['config']), $inputOutput['newValue']);
-//        }
+        $config = new Config();
+
+        $inputOutputs = [
+            [
+                'config'   => 'ROOT_NAMESPACE',
+                'newValue' => 'CodeJetter',
+            ],
+        ];
+
+        // this is for empty input
+        //$this->setExpectedException('Exception');
+
+        foreach ($inputOutputs as $inputOutput) {
+            // set with the new value
+            $config->set($inputOutput['config'], $inputOutput['newValue']);
+
+            $this->assertEquals($config->get($inputOutput['config']), $inputOutput['newValue']);
+        }
     }
 }
